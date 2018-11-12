4333d89658f93f8cd7f3944c74e1fa5cf3e7f742
Pujan Bajra <pujanisme@gmail.com>
2018-11-12 12:12:30 +0545

    Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,14 +8,37 @@ import { Component, OnInit } from '@angular/core';
   // Selecting Using Class
   // selector: '.app-servers',
 
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
+  // `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created';
+  serverName = '';
+  serverCreated = false;
+  servers = ['testserver', 'testserver1'];
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server Was Created With Name: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    // this.serverName = event.target.value; event: any
+    this.serverName = (<HTMLInputElement>event.target).value; // event: Event
+  }
 }
